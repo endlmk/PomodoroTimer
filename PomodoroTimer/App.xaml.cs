62a502fc51e0c4d3125d8b3e1af8a7d03951b258
kmishima <kmishima@test.com>
2016-09-12 23:55:25 +0900

    Without BootStrapper

--- PomodoroTimer/App.xaml.cs
+++ PomodoroTimer/App.xaml.cs
@@ -4,7 +4,10 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Prism.Mvvm;
 using System.Windows;
+using PomodoroTimer.Views;
 
 namespace PomodoroTimer
 {
@@ -13,12 +16,13 @@ namespace PomodoroTimer
     /// </summary>
     public partial class App : Application
     {
+        private IUnityContainer Container { get; } = new UnityContainer();
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var bootstrapper = new Bootstrapper();
-            bootstrapper.Run();
+            ViewModelLocationProvider.SetDefaultViewModelFactory(x => this.Container.Resolve(x));
+            this.Container.Resolve<MainWindow>().Show();
         }
     }
 }
